a268454c4ed7b56b6be381912032f6adaea8b651
jiwoongkim233 <rlawldnd841@gmail.com>
2023-10-10 18:08:19 +0900

    231010_insert

--- insert/insert.php
+++ insert/insert.php
@@ -1,23 +1,33 @@
+<!-- xcopy D:\workspace\project1 C:\Apache24\htdocs\project1 /E /Y -->
 <!DOCTYPE html>
 <html lang="ko">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="/insert.css">
+	<link rel="stylesheet" href="insert.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300&family=Noto+Serif+KR:wght@200&display=swap" rel="stylesheet">
-	<title>Document</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Nanum+Pen+Script&family=Noto+Sans+KR:wght@300;400&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="../list/header.css">
+<link rel="stylesheet" href="../list/status.css">
+
+<title>Document</title>
 	
 </head>
 <body>
+	<?php
+;
+	require_once("../list/header.html");
+	require_once("../list/status.html");
+	?>
 <form action="">
 	<div class="container">
-		<p>2023년 10월 10일</p>
+		<p class="create_at">2023년 10월 10일</p>
 
-			<button class="div_1">
+			<button class="div_1 div_css">
 				<h3>건강한 아침 챌린지</h3>
-				<br><br>		
+				<br>		
 					스트레칭 하기 0/1
 					<br>
 					물 마시기 0/1
@@ -27,9 +37,9 @@
 					아침 식사 0/1
 				</button>
 
-			<button class="div_2">
+			<button class="div_2 div_css">
 				<h3>좋은 수면 챌린지</h3> 
-				<br><br>
+				<br>
 					스트레칭 하기 0/1
 					<br>
 					자기 전 카페인 섭취하지 않기 0/1
@@ -39,10 +49,9 @@
 					독서하기 0/1		
 			</button>
 				<br>
-				<button class="div_3">
+				<button class="div_3 div_css">
 					<h3>뇌 건강 챌린지</h3>
-					<br><br>
-					
+					<br>
 					20분 산책하기 0/1
 					<br>
 					견과류 먹기 0/1 
@@ -51,10 +60,9 @@
 					<br>
 					일기 쓰기 0/1
 				</button>
-			<button class="div_4">
+			<button class="div_4 div_css">
 					<h3>다이어트 챌린지</h3>
-					<br><br>
-					
+					<br>
 					물 2리터 이상 마시기 0/1
 					<br>
 					매끼 단백질 섭취 0/1
@@ -64,9 +72,9 @@
 					하루 10000보 걷기
 			</button>
 			<br>
-			<button class="div_5">
+			<button class="div_5 div_css">
 					<h3>갓생살기</h3> 
-					<br><br>
+					<br>
 					<div class="div_content">
 					하루 sns 금지 0/1
 					<br>
