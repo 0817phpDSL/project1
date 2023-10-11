553c6742a627842b699e4d2c9033a86f178ff1af
mk1206 <alsrod1400@naver.com>
2023-10-11 16:09:31 +0900

    1011_update

--- detail/in-progress.php
+++ detail/in-progress.php
@@ -1,6 +1,25 @@
 <?php
 
+require_once($_SERVER["DOCUMENT_ROOT"]."/project1/list/bar_lib.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/project1/detail/in_lib.php");
 
+if(!my_db_conn($conn)) {
+	// DB Instance 에러
+	throw new Exception("DB Error : PDO Instance");
+}
+
+$http_method = $_SERVER["REQUEST_METHOD"];
+$arr_post = $_POST;
+
+$arr_post["create_id"] = isset($arr_post["create_id"]) ? $arr_post["create_id"] : "1";
+print_r($arr_post);
+
+$list = db_select_list($conn, $arr_post);
+if($list === false) {
+	// DB Instance 에러
+	throw new Exception("list Error");
+
+}
 
 ?>
 
@@ -14,26 +33,26 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Nanum+Pen+Script&family=Noto+Sans+KR:wght@300;400&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="../list/header.css">
+	<!-- <link rel="stylesheet" href="../list/header.css"> -->
 	<link rel="stylesheet" href="../list/status.css">
 	<link rel="stylesheet" href="../list/challenge_bar.css">
 	<title>Document</title>
 </head>
 <body>
 	<?php
-    require_once("../list/header.html");
+    // require_once("../list/header.html");
     require_once("../list/status.html");
-	require_once("../list/challenge_bar.html");
+	require_once("../list/challenge_bar.php");
     ?>
 	<section class="section-in">
 		<form class="form-in" action="">
 			<p class="create_at">2023년 10월 10일</p>
 			<p class="ch-name">건강한 아침</p>
 			<progress class="progress" value="50" max="100"></progress>
-			<button class="button-in"><p class="pro-menu">스트레칭하기</p> <p class="pro-clear">1/1</p></button>
-			<button class="button-in"><p class="pro-menu">물 마시기</p> <p class="pro-clear">0/1</p></button>
-			<button class="button-in"><p class="pro-menu">햇볕 쬐기</p> <p class="pro-clear">0/1</p></button>
-			<button class="button-in"><p class="pro-menu">아침식사</p> <p class="pro-clear">0/1</p></button>
+			<?php
+			foreach($list as $item) { ?>
+			<button class="button-in"><p class="pro-menu"><?php echo $item["l_name"] ?></p> <p class="pro-clear">1/1</p></button>
+		<?php } ?>
 			<a href=""><img class="trash" src="../src/trash.png" alt=""></a>
 		</form>
 	</section>
--- list/challenge_bar.php
+++ list/challenge_bar.php
@@ -12,14 +12,10 @@ try {
         throw new Exception("DB Error : PDO Instance"); // 강제 예외발생 : DB Instance
     }
 
-	$challenge = db_select_challenge($conn);
-	if($challenge === false) {
+    $challenge_bar = db_select_challenge_bar($conn);
+    if($challenge_bar === false) {
 		throw new Exception("select_challenge Error");
 	}
-
-    $challenge[]
-    
-
 
 } catch(Exception $e) {
     echo $e->getMessage(); // 예외발생 메세지 출력
@@ -30,21 +26,19 @@ try {
 
 ?>
 
-<!-- <link rel="stylesheet" href="challenge_bar.css"> -->
+<link rel="stylesheet" href="challenge_bar.css">
 <div class="challenge_bar box1">
-<form action="">
-<header class="challenge_header">
-<p class="challenge_title">Challenge</p>
-<button class="insert_button">+</button>
-</header>
-<section>
-<?php
-foreach($challenge as $item) {
-    for($i = 0; $i <= ; $i++) { ?>
-    <button class="challenge_list_select challenge_list_shadow"><?php echo ;?></button> <br>
-    <?php
-    } 
-    } ?>
-</section>
-</form>
+    <form action="/project1/detail/in-progress.php" method="post">
+        <header class="challenge_header">
+            <p class="challenge_title">Challenge</p>
+            <button class="insert_button">+</button>
+        </header>
+        <section>
+            <?php
+            foreach($challenge_bar as $item) {  ?>
+                    <button class="challenge_list_select challenge_list_shadow" name="create_id" value="<?php echo $item["create_id"];?>"><?php echo $item["c_name"]; ?></button> <br>
+                <?php }
+            ?>
+        </section>
+    </form>
 </div>
