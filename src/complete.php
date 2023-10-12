efc6eb56f39f8cedc8f6a7794b66b5160928f968
jueunyang08 <sa04129@naver.com>
2023-10-12 18:22:17 +0900

    com 수정

--- src/complete.php
+++ src/complete.php
@@ -1,6 +1,9 @@
 <!-- xcopy D:\workspace\project1 C:\Apache24\htdocs\project1 /E /Y -->
 <?php
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/project1/src/"); //웹 서버
+define("FILE_HEADER", ROOT."html/header.html");
+define("FILE_STATUS", ROOT."html/status.html");
+define("FILE_CHALLENGE", ROOT."challenge_bar.php");
 require_once(ROOT. "lib/com_lib.php"); // DB 라이브러리
 require_once(ROOT. "lib/bar_lib.php"); // DB 라이브러리
 // require_once("")
@@ -125,21 +128,21 @@ finally {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/complete.css">
+    <link rel="stylesheet" href="/project1/src/css/complete.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Nanum+Pen+Script&family=Noto+Sans+KR:wght@300;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/status.css">
-    <link rel="stylesheet" href="css/challenge_bar.css">
+    <link rel="stylesheet" href="/project1/src/css/header.css">
+    <link rel="stylesheet" href="/project1/src/css/status.css">
+    <link rel="stylesheet" href="/project1/src/css/challenge_bar.css">
     
     
 </head>
 <body>
     <?php
-    require_once(ROOT."html/header.html");
-    require_once(ROOT."html/status.html");
-    require_once(ROOT."challenge_bar.php");
+    require_once(FILE_HEADER);
+	require_once(FILE_STATUS);
+	require_once(FILE_CHALLENGE);
     // var_dump($result);
     ?>
     <section class="section-in">
