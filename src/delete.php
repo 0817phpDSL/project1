a9e477d133997f9c809f38893d3e044fffa24c73
gaebaljoa <wjdwldn156@naver.com>
2023-10-13 11:33:01 +0900

    1013_delete

--- src/delete.php
+++ src/delete.php
@@ -13,6 +13,7 @@ require_once(ROOT."lib/delete_lib.php"); // DB관련 라이브러리
 // $page_flg = [
 // 	"page_flg" => $page_flg
 // ];
+$arr_get = [];
 
 $conn = null;
 
@@ -20,35 +21,60 @@ $conn = null;
 	if(!my_db_conn($conn)) {
 		throw new Exception("DB Error : PDO Instance");
 	}
+	
+	
+	
+	
+	$http_method = $_SERVER["REQUEST_METHOD"];
 
-
-
-
-	// $http_method = $_SERVER["REQUEST_METHOD"];
-
-	// method가 get일때. 게시글 조회하는 동작.
-	// if($http_method === "GET") {
-	// 	$create_id = isset($_GET["create_id"]) ? $_GET["create_id"] : "";
-	// 	$page_flg = isset($_GET["page_flg"]) ? $_GET["page_flg"] : "";
-
-		$arr_param = [
-			"create_id" => "10"
-		];
-		$result = db_select_boards_id($conn, $arr_param);
-
+	if($http_method === "GET") {
+		$arr_get = $_GET;
+		
+		$result = db_select_boards_id($conn, $arr_get);
+	
 		// 예외 처리
 		if($result === false) {
 			throw new Exception("DB Error : Select id");
 		}
+	} else {
+		try{
+			$arr_post = $_POST;
 
+			$conn->beginTransaction();
+
+			if(!db_delete_boards_id($conn, $arr_post)) {
+				throw new Exception("delete Error");
+			}
+			
+			$conn->commit();
+			if($arr_post["page_flg"] === "0") {
+				header("Location: in-progress.php");
+			} else if($arr_post["page_flg"] === "1") {
+				header("Location: complete.php");
+			}
+			exit;
+		} catch(Exception $e) {
+			$conn->rollBack();
+			echo $e->getMessage();
+			exit;
+		}
+	}
+	
+	// method가 get일때. 게시글 조회하는 동작.
+	// if($http_method === "GET") {
+		// 	$create_id = isset($_GET["create_id"]) ? $_GET["create_id"] : "";
+		// 	$page_flg = isset($_GET["page_flg"]) ? $_GET["page_flg"] : "";
+		
+
+
+
+		// print_r($arr_get);
+		// print_r($result);
 		print_r($_POST);
-		print_r($_GET);
 		// } else if(!(count($result) === 1)) {
 		// 	// result가 true면 갯수를 세고, 갯수가 1이 아니면 예외발생?
 		// 	throw new Exception("DB Error : Select id Count");
 		// }
-
-		$item = $result[0];
 	// } else {
 	// 	$create_id = isset($_POST["create_id"]) ? $_POST["create_id"] : "";
 
@@ -102,10 +128,15 @@ $conn = null;
 				</tr>
 				<tr>
 					<td>
-					<form action="/project1/src/delete.php" method="post">
-						<!-- <input type="hidden" name="id" value=""> -->
-						<button class="btn1" type='submit' name="create_id" value="<?php echo $item["create_id"] ?>">ㅇㅇ..</button>
-						<a class="btn2" href="/project1/src/detail.php/">ㄴㄴ..</a>
+					<form action="delete.php" method="post">
+						<input type="hidden" name="page_flg" value="<?php echo $arr_get["page_flg"] ?>">
+						<button class="btn1" type='submit' name="create_id" value="<?php echo $result[0]["create_id"] ?>">ㅇㅇ..</button>
+						<a class="btn2" href="<?php if($arr_get["page_flg"] === "0") {
+							echo "in-progress.php";
+						} else if($arr_get["page_flg"] === "1") {
+							echo "complete.php";
+						} 
+						?>">ㄴㄴ..</a>
 					</form>
 					</td>
 				</tr>
