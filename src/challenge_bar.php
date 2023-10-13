60a639a6ebf5b6aff93920b7453a52e6141ec398
mk1206 <alsrod1400@naver.com>
2023-10-13 12:44:22 +0900

    1013_color

--- src/challenge_bar.php
+++ src/challenge_bar.php
@@ -36,10 +36,10 @@ try {
             <?php
             foreach($challenge_bar as $item) {
                 if($item["c_com_at"] == null) { ?>
-                    <button class="challenge_list_not_select challenge_list_shadow" name="create_id" value="<?php echo $item["create_id"];?>"><?php echo $item["c_name"]; ?></button> <br>
+                    <button class="challenge_list_not_select challenge_list_shadow <?php echo isset($in_progress_c_id) && $in_progress_c_id == $item["create_id"]? "challenge_highlight" : "" ?>" name="create_id" value="<?php echo $item["create_id"];?>"><?php echo $item["c_name"]; ?></button> <br>
                 <?php }
                 if($item["c_com_at"] != "") { ?>
-                    <button class="challenge_list_select_2 challenge_list_shadow" name="create_id" value="<?php echo $item["create_id"];?>"><?php echo $item["c_name"]; ?></button> <br>
+                    <button class="challenge_list_select_2 challenge_list_shadow <?php echo isset($in_progress_c_id) && $in_progress_c_id == $item["create_id"]? "challenge_highlight" : "" ?>" name="create_id" value="<?php echo $item["create_id"];?>"><?php echo $item["c_name"]; ?></button> <br>
                 <?php }
             }
             ?>
